af9b9c0f528e3128698b4b83fa9bac37d6c91a4f
igorrochap <RochaIgorP@hotmail.com>
2020-11-25 10:58:21 -0300

    Requisitos funcionais 1 e 2 implementados

--- Main.java
+++ Main.java
@@ -8,19 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("SISTEMA DE PRODUTIVIDADE ACADÊMICA");
         
-        Colaborador colab = new Aluno("Jorge", "jorge@jorge", "Graduação");
-        Colaborador colab1 = new Aluno("Mario", "jorge@jorge", "Graduação");
-        Colaborador colab2 = new Aluno("Silvio", "jorge@jorge", "Graduação");
-        Colaborador colab3 = new Professor("Marcos", "email");
-        
-        ProducaoAcademica prod = new ProducaoAcademica();
 
-        //prod.add(colab);
-        //prod.add(colab1);
-        //prod.add(colab2);
-        float valor = 100;
-        Projeto prj = new Projeto("titulo", "12/12/2012", "01/01/2013", "Caixa economica", valor, "Torre", "Teste", colab3);
-        
-        prj.alocaColaborador(colab);
     }
 }
